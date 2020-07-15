760d012a3c1e969311f498062c1169806b0dbd8d
Subhasis Ray <ray.subhasis@gmail.com>
2020-07-15 13:00:31 -0400

    Added docs for batch processor and a few more enhancements/updates.
    - implemented saving data
    - added option to disable CUDA
    - testing various ways of using the multiprocessing module.
    - so far CPU-based computation is pathetic (25 frames in 47 seconds using 5 processes with chunk size 5)
    - compared to this 0.5 FPS I get 6 FPS with CUDA.

--- argos/batchtrack.py
+++ argos/batchtrack.py
@@ -15,6 +15,7 @@ import numpy as np
 import cv2
 import yaml
 import time
+import pandas as pd
 import torch
 import torch.multiprocessing as mp
 import torch.backends.cudnn as cudnn
@@ -25,12 +26,12 @@ from yolact.utils.augmentations import FastBaseTransform
 from yolact.layers import output_utils as oututils
 
 logging.basicConfig(stream=sys.stdout,
-                        format='%(asctime)s '
-                               'p=%(processName)s[%(process)d] '
-                               't=%(threadName)s[%(thread)d] '
-                               '%(filename)s#%(lineno)d:%(funcName)s: '
-                               '%(message)s',
-                        level=logging.DEBUG)
+                    format='%(asctime)s '
+                           'p=%(processName)s[%(process)d] '
+                           't=%(threadName)s[%(thread)d] '
+                           '%(filename)s#%(lineno)d:%(funcName)s: '
+                           '%(message)s',
+                    level=logging.INFO)
 
 try:
     mp.set_start_method('spawn')
@@ -38,12 +39,13 @@ except RuntimeError:
     pass
 
 
-def init_yolact(cfgfile, netfile):
+def init_yolact(cfgfile, netfile, cuda):
     load_config(cfgfile)
-    load_weights(netfile)
+    load_weights(netfile, cuda)
 
 
-def process(frame_data: Tuple[int, np.ndarray], cuda: bool, score_threshold: float, top_k: int):
+def process(frame_data: Tuple[int, np.ndarray], cuda: bool,
+            score_threshold: float, top_k: int):
     """:returns (classes, scores, boxes)
 
     where `boxes` is an array of bounding boxes of detected objects in
@@ -117,13 +119,13 @@ def load_config(filename):
             config.mask_proto_debug = False
 
 
-def load_weights(filename):
+def load_weights(filename, cuda):
     global ynet
     if filename == '':
         raise ValueError('Empty filename for network weights')
     tic = time.perf_counter_ns()
     with torch.no_grad():
-        if torch.cuda.is_available():
+        if cuda:
             cudnn.fastest = True
             torch.set_default_tensor_type('torch.cuda.FloatTensor')
         else:
@@ -135,21 +137,53 @@ def load_weights(filename):
     toc = time.perf_counter_ns()
     logging.debug(f'Time to load weights: {1e-9 * (toc - tic)}')
 
+
 class BatchTrack(object):
     def __init__(self, video_filename, output_filename, config_filename,
-                 weights_filename, score_threshold, top_k):
-        self.batch_size = mp.cpu_count() * 4
-        self.read_lock = threading.Lock()
-        self.write_lock = threading.Lock()
+                 weights_filename, score_threshold, top_k,
+                 processes=4, batch_size=100, cuda=None):
+        """
+        Parameters
+        -------------
+        video_filename: str
+            path to video file to process
+        output_filename: str
+            path to output file. If the extension is ``csv`` then write in
+            comma separated text file. If ``hdf`` or ``h5`` then write in hdf5
+            format.
+        config_filename: str
+            Yolact configuration file path
+        weights_filename: str
+            path to file containing trained network weights
+        score_threshold: float
+            threshold for detection score
+        top_k: int
+            keep only the top ``top_k`` detections by score
+        processes: int
+            number of processes to run in parallel. If ``None`` then it is one
+            less than the CPU count.
+        batch_size: int
+            each process receives this many frames in each round
+        cuda: bool
+            whether or not to use GPU. If ``None`` detect automatically.
+        """
+        self.batch_size = batch_size
         self.video = cv2.VideoCapture(video_filename)
         if (self.video is None) or not self.video.isOpened():
             raise IOError('Could not open video')
         self.output_filename = output_filename
-        self.cuda = False   # torch.cuda.is_available()
+        if cuda is None:
+            self.cuda = torch.cuda.is_available()
+        else:
+            self.cuda = cuda
         self.score_threshold = score_threshold
         self.top_k = top_k
         self.config_filename = config_filename
         self.weights_filename = weights_filename
+        if processes is None:
+            self.processes = mp.cpu_count() - 1
+        else:
+            self.processes = processes
 
     def read_frame(self):
         if (self.video is None) or not self.video.isOpened():
@@ -164,34 +198,63 @@ class BatchTrack(object):
         process_partial = partial(process, cuda=self.cuda,
                                   score_threshold=self.score_threshold,
                                   top_k=self.top_k)
-        results = {}
-        with mp.Pool(initializer=init_yolact, initargs=(self.config_filename, self.weights_filename)) as pool:
-            while True:
+        results = []
+        frame_count = 0
+        with mp.Pool(processes=self.processes, initializer=init_yolact,
+                     initargs=(self.config_filename, self.weights_filename,
+                               self.cuda)) as pool:
+            tic = time.perf_counter_ns()
+            vid_end = False
+            while not vid_end:
                 frames = []
-                for ii in range(self.batch_size):
+                t_0 = time.perf_counter_ns()
+                for ii in range(self.batch_size * self.processes):
                     frame_no, frame = self.read_frame()
                     if frame is None:
                         vid_end = True
-                    frames.append((frame_no, frame))
+                        break
+                    else:
+                        frames.append((frame_no, frame))
                 if len(frames) == 0:
                     break
-                result = pool.imap_unordered(process_partial, frames,
-                                            chunksize=10)
+                result = pool.map(process_partial, frames,
+                                             chunksize=self.batch_size)
                 for ret in result:
-                    results[ret[0]] = ret[1]
-                if vid_end:
-                    break
+                    results += [{'frame': ret[0], 'x': ret[1][ii, 0],
+                                 'y': ret[1][ii, 1],
+                                 'w': ret[1][ii, 2],
+                                 'h': ret[1][ii, 3]} for ii in
+                                range(ret[1].shape[0])]
+                frame_count += len(frames)
+                t_1 = time.perf_counter_ns()
+                logging.info(f'Processed {frame_count} frames in '
+                             f'{1e-9 * (t_1 - t_0)} seconds using '
+                             f'{self.processes} processes.')
+            toc = time.perf_counter_ns()
+            logging.info(
+                f'Processed {frame_count} frames in {(toc - tic) * 1e-9} seconds.'
+                f' FPS={frame_count * 1e9 / (toc - tic)}')
+        tic = time.perf_counter_ns()
+        results = pd.DataFrame(results)
+        results = results.sort_values(by='frame')
+        if self.output_filename.endswith('.csv'):
+            results.to_csv(self.output_filename, index=False)
+        elif self.output_filename.endswith(
+                '.h5') or self.output_filename.endswith('.hdf'):
+            results.to_hdf(self.output_filename, 'track')
+        toc = time.perf_counter_ns()
+        logging.info(f'Saved {len(results)} bboxes '
+                     f'in {(toc - tic) * 1e-9} seconds.'
+                     f' At {len(results) * 1e9 / (toc - tic)}'
+                     f' rows (bboxes) per second.')
 
 
 if __name__ == '__main__':
-
-    track = BatchTrack(video_filename='C:/Users/raysu/Documents/src/argos_data/dump/2020_02_20_00270.avi',
-    output_filename='C:/Users/raysu/Documents/src/argos_data/dump/2020_02_20_00270.avi.track.csv',
-                       config_filename='C:/Users/raysu/Documents/src/argos_data/yolact_annotations/yolact_config.yaml',
-                       weights_filename='C:/Users/raysu/Documents/src/argos_data/yolact_annotations/babylocust_resnet101_119999_240000.pth',
-                       score_threshold=0.15, top_k=10)
+    # logging.getLogger().setLevel(logging.INFO)
+    track = BatchTrack(
+        video_filename='C:/Users/raysu/Documents/src/argos_data/dump/2020_02_20_00270.avi',
+        output_filename='C:/Users/raysu/Documents/src/argos_data/dump/2020_02_20_00270.avi.parallel.track.csv',
+        config_filename='C:/Users/raysu/Documents/src/argos_data/yolact_annotations/yolact_config.yaml',
+        weights_filename='C:/Users/raysu/Documents/src/argos_data/yolact_annotations/babylocust_resnet101_119999_240000.pth',
+        score_threshold=0.15, top_k=10, cuda=False, processes=5, batch_size=5)
     track.start()
-
-
-
-
