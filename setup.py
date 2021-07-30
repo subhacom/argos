6bf93c0e55dd161a01a909e1e9333fab1fe267cf
Subhasis Ray <ray.subhasis@gmail.com>
2021-07-30 09:38:23 -0400

    Version bumped up.

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ ext_modules = [
 ]
 setup(
     name='argos_toolkit',
-    version='0.1.0-13',
+    version='0.1.0-14',
     author='Subhasis Ray',
     author_email='ray.subhasis@gmail.com',
     description='Software utility for tracking multiple objects (animals) in a video.',
